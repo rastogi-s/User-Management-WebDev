abdf14e7006ea45d498f989009938e7474afff93
rastogi-s <35542305+rastogi-s@users.noreply.github.com>
2018-05-09 21:10:41 -0400

    changes made in admin template,style and controller

--- src/main/webapp/jquery/components/admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/admin/user-admin.controller.client.js
@@ -1,28 +1,79 @@
-(function() {
-	var $usernameFld, $passwordFld;
-	var $removeBtn, $editBtn, $createBtn;
-	var $firstNameFld, $lastNameFld;
-	var $userRowTemplate, $tbody;
-	var userService = new AdminUserServiceClient();
-	$(main);
+//IIFE
+(function () {
+    var $usernameFld, $passwordFld, $roleFld;
+    var $removeBtn, $editBtn, $createBtn;
+    var $searchBtn, $updateBtn;
+    var $firstNameFld, $lastNameFld;
+    var $userRowTemplate, $tbody;
+    //var userService = new AdminUserServiceClient();
+    $(main);
 
-	function main() {
-		
-	}
-	function createUser() {
-	}
-	function findAllUsers() {
-	}
-	function findUserById() {
-	}
-	function deleteUser() {
-	}
-	function selectUser() {
-	}
-	function updateUser() {
-	}
-	function renderUser(user) {
-	}
-	function renderUsers(users) {
-	}
+    function main() {
+        //alert($usernameFld);
+        $usernameFld = $('#usernameFld');
+        $passwordFld = $('#passwordFld');
+        $firstNameFld = $('#firstNameFld');
+        $lastNameFld = $('#lastNameFld');
+        $roleFld = $('#roleFld');
+        $editBtn = $('#wbdev-edit');
+        $removeBtn = $('#wbdev-remove');
+        $searchBtn = $('.wbdev-search');
+        $createBtn = $('.wbdev-create')
+        $updateBtn = $('.wbdev-update');
+        $userRowTemplate = $('.wbdev-user');
+        $tbody = $('.table-body');
+        $createBtn.click(function () {
+            createUser();
+        });
+        $searchBtn.click(function () {
+            findUserById();
+        });
+        $removeBtn.click(function () {
+            deleteUser();
+        });
+        $editBtn.click(function () {
+            updateUser();
+        });
+
+        $updateBtn.click(function () {
+
+        });
+
+
+
+    }
+
+    function createUser() {
+        alert('create user');
+    }
+
+    function findAllUsers() {
+        alert('find all users');
+    }
+
+    function findUserById() {
+        alert('find user by id');
+    }
+
+    function deleteUser() {
+        alert('remove user');
+    }
+
+    function selectUser() {
+        alert('select user');
+    }
+
+    function updateUser() {
+        alert('update user');
+    }
+
+    function renderUser(user) {
+        alert('render single user');
+    }
+
+    function renderUsers(users) {
+        alert('render all users');
+    }
 })();
+
+
